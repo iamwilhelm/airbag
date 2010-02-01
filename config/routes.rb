33c1df82c3ce4f9f12b64d9a0ace8c2769024d2e
Wil Chung <iamwil@gmail.com>
2010-01-31 21:54:02 -0800

    fixed home link button

--- config/routes.rb
+++ config/routes.rb
@@ -30,14 +30,14 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
   # See how all your routes lay out with "rake routes"
+
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  map.root :controller => "funnelcake", :action => "index"
 
   # doorway routes
   map.with_options :controller => "funnelcake" do |funnelcake|
-    funnelcake.home "", :action => "index"
+    funnelcake.home_funnelcake "", :action => "index"
   end
 
   # visualization routes
