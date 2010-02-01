81256b0b3219e78e514e2a034e238ce9786c21ad
Wil Chung <iamwil@gmail.com>
2010-02-01 01:13:52 -0800

    Minor comments

--- app/controllers/funnelcake_controller.rb
+++ app/controllers/funnelcake_controller.rb
@@ -1,28 +1,35 @@
 class FunnelcakeController < ApplicationController
   layout "doorway"
 
+  # GET
   # The frontpage
   def index    
   end
 
+  # GET
   # Shows user the benefits of using the app
   def benefits
   end
 
+  # GET
   # Where the user can go for help
   def help
   end
 
+  # GET
   # More details about us
   def about
   end
 
+  # GET
   # If people want to give feedback
   def feedback
   end
 
+  # POST
   # signs up to be notified when we launch
-  def notify
+  def launch_notify
+    
   end
 
 end
