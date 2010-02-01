d8f98d4d9db75a8e198ba155f844aa8329ac8bca
Wil Chung <iamwil@gmail.com>
2010-01-31 19:48:52 -0800

    added missing partials for visualization index

--- app/controllers/viz_controller.rb
+++ app/controllers/viz_controller.rb
@@ -2,11 +2,11 @@ class VizController < ApplicationController
   layout "visualization"
   
   def index
-    
+    params[:dimension] || "price_of_beverage"
   end
 
   def show
-    
+    render :json => { "hello" => params[:id] }
   end
   
 end
