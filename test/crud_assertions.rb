b3b496e027e1fa72ca39ae3304069cde70981959
Wil Chung <iamwil@gmail.com>
2010-02-12 12:41:03 -0800

    changed crud assertions to be more flexible.

--- test/crud_assertions.rb
+++ test/crud_assertions.rb
@@ -6,8 +6,8 @@ module CrudAssertions
     assert_template page_name.to_s
   end  
 
-  def assert_created(model_name, &block)
-    assert_assigns_proc(1).call(model_name, block)
+  def assert_created(model_name, number_created = 1, &block)
+    assert_assigns_proc(number_created).call(model_name, block)
   end
 
   def assert_updated(model_name, &block)
@@ -15,7 +15,7 @@ module CrudAssertions
   end
 
   def assert_destroyed(model_name, &block)
-    assert_assigns_proc(-1, datasources_path).call(model_name, block)
+    assert_assigns_proc(-1).call(model_name, block)
   end
   
   def assert_contains(model_name, attributes_hash)
@@ -26,13 +26,11 @@ module CrudAssertions
 
   private
   
-  def assert_assigns_proc(number, redirect_path = nil)
+  def assert_assigns_proc(number)
     proc do |model_name, block|
       assert_difference("#{model_name.to_s.classify}.count", number) do
         block.call
-        redirect_path ||= datasource_path(assigns(model_name))
         assert assigns(model_name)
-        assert_redirected_to redirect_path
       end
     end
   end
