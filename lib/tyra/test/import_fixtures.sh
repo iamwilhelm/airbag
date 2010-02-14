4feedd41b56c30d4b7bf84e2a64e1a5d4889db7b
ian <ian@ian1.(none)>
2010-02-14 15:25:53 -0500

    flush dbs before importing test data

--- lib/tyra/test/import_fixtures.sh
+++ lib/tyra/test/import_fixtures.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "--clearing test db--"
+redis-cli -n 2 flushdb
+redis-cli -n 3 flushdb
 echo "--importing data--"
 cd ..
 find test/fixtures -name "*.csv" -print0 | xargs -0 -n 1 ./tyra.rb -n 2 -i
