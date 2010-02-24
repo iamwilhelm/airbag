bfd143df30853fccb202010e962e095b0dd30b84
Wil Chung <iamwil@gmail.com>
2010-02-24 15:07:46 -0800

    bugfix: email settings need the keys symbolized in order for it to take.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -3,7 +3,8 @@ require 'yaml'
 # loads the email settings from config/email.yml
 settings = YAML.load_file(File.join(RAILS_ROOT, "config", "email.yml"))
 unless settings[RAILS_ENV].nil? or settings[RAILS_ENV].empty?
-  ActionMailer::Base.delivery_method = settings[RAILS_ENV].delete("delivery_method")
+  settings[RAILS_ENV].symbolize_keys!
+  ActionMailer::Base.delivery_method = settings[RAILS_ENV].delete(:delivery_method)
   ActionMailer::Base.smtp_settings = settings[RAILS_ENV]
 else
   ActionMailer::Base.delivery_method = :test
