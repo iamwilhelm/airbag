cf32fcb5af588f2425aba317cb7172be23511f9c
Wil Chung <iamwil@gmail.com>
2010-02-24 17:50:21 -0800

    minor variable name change

--- app/models/sales_mailer.rb
+++ app/models/sales_mailer.rb
@@ -1,12 +1,13 @@
 class SalesMailer < ActionMailer::Base
   layout 'email'
   
-  def notify_when_launch_email(email)
+  def notify_when_launch_email(email_address)
     recipients Graphbug::DEVELOPER_EMAILS
     from "graphbug@graphbug.com"
     subject "[Graphbug] Request for launch notification"
     sent_on Time.now
-    body({ :email => email })
+    body({ :email => email_address })
+  end
   end
   
 end
