731f42178a7b3aebb67b95e4b7e4068617cef18b
Wil Chung <iamwil@gmail.com>
2010-03-03 16:59:16 -0800

    bugfix: column checkboxes aren't being selected

--- app/helpers/datatables_helper.rb
+++ app/helpers/datatables_helper.rb
@@ -3,7 +3,7 @@ module DatatablesHelper
 
   # for datatable row and column selection form
   def column_checked?(datatable, col_index)
-    datatable.new_record? or (datatable.column_checked?(col_index))
+    datatable.new_record? or datatable.column_checked?(col_index)
   end
 
   def row_checked?(datatable, row_index)
